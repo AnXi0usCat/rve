f93f0fd7a2faffcbdeace1256d5edb2a2e49bd93
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2025-06-08 17:47:46 +0100

    finish basic proxy server

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,9 +1,10 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, HttpResponse, HttpServer, web};
 use serde::{Deserialize, Serialize};
 pub mod service {
     tonic::include_proto!("proxy");
 }
 use service::{PredictRequest, proxy_service_client::ProxyServiceClient};
+use tonic::Request;
 
 #[derive(Deserialize)]
 struct HttpPredictRequest {
@@ -17,11 +18,28 @@ struct HttpPredictResponse {
     message: String,
 }
 
-async fn predict_handler(json: web::Json<HttpPredictRequest>) -> impl Responder {
-        HttpResponse::Ok().json(HttpPredictResponse {
-        status:  String::from("200"),
-        message: String::from("hello"),
-    })
+async fn predict_handler(
+    json: web::Json<HttpPredictRequest>,
+) -> Result<HttpResponse, actix_web::Error> {
+    let mut client = ProxyServiceClient::connect("http://[::1]:50051")
+        .await
+        .map_err(actix_web::error::ErrorInternalServerError)?;
+
+    let request = Request::new(PredictRequest {
+        user_id: json.user_id.clone(),
+        action: json.action.clone(),
+    });
+
+    let response = client
+        .predict(request)
+        .await
+        .map_err(actix_web::error::ErrorInternalServerError)?
+        .into_inner();
+
+    Ok(HttpResponse::Ok().json(HttpPredictResponse {
+        status: response.status,
+        message: response.message,
+    }))
 }
 
 #[actix_web::main]
