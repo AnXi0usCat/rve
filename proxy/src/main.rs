4c53c63a31b2f141c7c192542006700f4c5b5f56
Mihhail Fjodorov <mikhail.fjodorov@gmail.com>
2025-06-08 16:59:09 +0100

    server skeleton

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,8 +1,33 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
 pub mod service {
     tonic::include_proto!("proxy");
 }
+use service::{PredictRequest, proxy_service_client::ProxyServiceClient};
+
+#[derive(Deserialize)]
+struct HttpPredictRequest {
+    user_id: String,
+    action: String,
+}
+
+#[derive(Serialize)]
+struct HttpPredictResponse {
+    status: String,
+    message: String,
+}
+
+async fn predict_handler(json: web::Json<HttpPredictRequest>) -> impl Responder {
+        HttpResponse::Ok().json(HttpPredictResponse {
+        status:  String::from("200"),
+        message: String::from("hello"),
+    })
+}
 
 #[actix_web::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/predict", web::get().to(predict_handler)))
+        .bind("0.0.0.0:8000")?
+        .run()
+        .await
 }
